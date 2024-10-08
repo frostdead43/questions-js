01262a50957f5c306bdaaeffe67d3e7a94d0100a
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2024-10-08 15:59:13 +0300

    tüm sorular icin localStorage eklendi

--- assets/questions.js
+++ assets/questions.js
@@ -5,21 +5,21 @@
 
 
 
-// let price = prompt("Enter price:");
+let price = prompt("Enter price:");
 
-// let discount = prompt("Enter discount rate(with % ):");
+let discount = prompt("Enter discount rate(with % ):");
 
-// let sale = ((price * discount) / 100);
+let sale = ((price * discount) / 100);
 
-// let lastPrice = price - sale;
+let lastPrice = price - sale;
 
-// if (lastPrice !== 0) {
-//     alert("Product Price:" + " "  + lastPrice );
-// }else if (lastPrice === "0" || " ") {
-//     alert("0 or invalid!");
-// }
+if (lastPrice !== 0) {
+    alert("Product Price:" + " "  + lastPrice );
+}else if (lastPrice === "0" || " ") {
+    alert("0 or invalid!");
+}
 
-// localStorage.setItem("question1", `${lastPrice}`);
+localStorage.setItem("question1", `${lastPrice}`);
 
 
 
@@ -29,24 +29,26 @@
 // Eğer not 50’den büyükse "Geçtiniz", 50'den küçükse ve 40’tan büyükse "Şartlı geçtiniz", 40’tan küçükse "Kaldınız" mesajını verin.
 
 
-// let vize = prompt("first exam: ");
+let vize = prompt("first exam: ");
 
-// let final = prompt("final exam: ");
+let final = prompt("final exam: ");
 
-// let average = (((vize * 40) / 100) + ((final * 60)/100));
+let average = (((vize * 40) / 100) + ((final * 60)/100));
 
 
-// if (average >= 50) {
-//     alert(average);
-//     alert("You passed this exam!");
-// } else if (average < 50 && average >= 40) {
-//     alert(average);
-//     alert("You have conditionally passed this exam!");
-// } else if (average < 40){
-//     alert(average);
-//     alert("You failed!");
+if (average >= 50) {
+    alert(average);
+    alert("You passed this exam!");
+} else if (average < 50 && average >= 40) {
+    alert(average);
+    alert("You have conditionally passed this exam!");
+} else if (average < 40){
+    alert(average);
+    alert("You failed!");
 
-// } else alert("invalid!"); 
+} else alert("invalid!"); 
+
+localStorage.setItem("question2", `${average}`);
 
 
 
@@ -54,80 +56,85 @@
 // Kullanıcıdan bir sayı alın ve o sayıya karşılık gelen günü yazdırın (1 = Pazartesi, 7 = Pazar). Switch case araştırabilirsiniz.
 
 
-// let number = Number(prompt("insert Number(1-7)"));
+let number = Number(prompt("insert Number(1-7)"));
 
-// switch (Number(number)) {
+switch (Number(number)) {
 
-//     case 1 :
-//         alert("Monday");
-//         break;
-//     case 2 :
-//         alert("Tuesday");
-//         break;
-//     case 3 :
-//         alert("Wednesday");
-//         break;
-//     case 4 :
-//         alert("Thursday");
-//         break;
-//     case 5 :
-//         alert("Friday");
-//         break;
-//     case 6 :
-//         alert("Saturday");
-//         break;
-//     case 7 :
-//         alert("Sunday");
-//         break;
+    case 1 :
+        alert("Monday");
+        break;
+    case 2 :
+        alert("Tuesday");
+        break;
+    case 3 :
+        alert("Wednesday");
+        break;
+    case 4 :
+        alert("Thursday");
+        break;
+    case 5 :
+        alert("Friday");
+        break;
+    case 6 :
+        alert("Saturday");
+        break;
+    case 7 :
+        alert("Sunday");
+        break;
 
-//     default:
-//         alert("invalid!");
-// }
+    default:
+        alert("invalid!");
+}
 
 
-
+localStorage.setItem("question3", `${number}`);
 
 
 // 4- Büyük Sayıyı Bulma:
 // Kullanıcıdan üç sayı alarak, bu sayılardan en büyüğünü bulan bir program yazın.
 
 
-// let number1 = Number(prompt("insert number 1"));
+let number1 = Number(prompt("insert number 1"));
 
-// let number2 = Number(prompt("insert number 2"));
+let number2 = Number(prompt("insert number 2"));
 
-// let number3 = Number(prompt("insert number 3"));
+let number3 = Number(prompt("insert number 3"));
 
-// if (number1 > number2 && number1 > number3) {
-//     alert(number1 + " " + "biggest");
+if (number1 > number2 && number1 > number3) {
+    alert(number1 + " " + "biggest");
 
-// } else if (number2 > number1 && number2 > number3) {
-//     alert(number2 + " " + "biggest");
+} else if (number2 > number1 && number2 > number3) {
+    alert(number2 + " " + "biggest");
 
-// } else if (number3 > number1 && number3 > number2) {
-//     alert(number3 + " " + "biggest");
+} else if (number3 > number1 && number3 > number2) {
+    alert(number3 + " " + "biggest");
 
-// } else console.log ("nothing!");
+} else console.log ("nothing!");
+
+localStorage.setItem("question4", `${number1} ${number2} ${number3}`);
 
 
 
 // 5- Saniyeyi Saate Çevirme:
 // Kullanıcıdan bir saniye değeri alıp bunu saat, dakika ve saniye cinsine çevirin (Örn: 3661 saniye = 1 saat, 1 dakika, 1 saniye).
 
-// let second = Number(prompt("enter random second"))
+let second = Number(prompt("enter random second"))
 
-// let convertHour = Math.floor(second / 3600);
-
-
-// let convertMinute = Math.floor(second / 60) % 60;
+let convertHour = Math.floor(second / 3600);
 
 
-// let convertSecond = Math.floor((second % 60));
+let convertMinute = Math.floor(second / 60) % 60;
+
+
+let convertSecond = Math.floor((second % 60));
 
 
 
 
-// alert (`${convertHour} Hour +  ${convertMinute} Minute +  ${convertSecond} Second`);
+alert (`${convertHour} Hour +  ${convertMinute} Minute +  ${convertSecond} Second`);
+
+localStorage.setItem("question5", `${convertHour} ${convertMinute} ${convertSecond}`);
+
 
 
 
@@ -165,7 +172,8 @@
     
 
     if (birthDay < thisDay) {
-        thisMonth +=1;     
+        birthDay % 30;  
+        thisMonth -=1;
     } else (thisMonth === thisMonth);
 
     totalResult = (totalYear  + " " + totalMonth + " " +  totalDay);
@@ -173,27 +181,34 @@
     console.log(totalResult);
 
 
+    localStorage.setItem("question6", `${totalResult}`);
+
+
 
 
 // 7- Saatlik Ücret Hesaplama:
 // Kullanıcıdan çalıştığı saat sayısını ve saat başı ücretini alarak toplam kazancını hesaplayın.
 
-// let userHourWage = prompt("Insert your hourly Wage ");
+let userHourWage = prompt("Insert your hourly Wage ");
 
-// let userWorkingHour = prompt("Insert How many hours you work per day");
+let userWorkingHour = prompt("Insert How many hours you work per day");
 
-// let dailyIncome = (userHourWage * userWorkingHour);
+let dailyIncome = (userHourWage * userWorkingHour);
 
-// let weeklyIncome = (dailyIncome * 6);
+let weeklyIncome = (dailyIncome * 6);
 
-// let monthlyIncome = (weeklyIncome * 4);
+let monthlyIncome = (weeklyIncome * 4);
 
-// let yearlyIncome = (monthlyIncome * 12);
+let yearlyIncome = (monthlyIncome * 12);
 
-// alert(" Daily income : "  + dailyIncome);
-// alert(" Weekly income : " + weeklyIncome);
-// alert(" Monthly income : " + monthlyIncome);
-// alert(" Yearly income : " + yearlyIncome);
+alert(" Daily income : "  + dailyIncome);
+alert(" Weekly income : " + weeklyIncome);
+alert(" Monthly income : " + monthlyIncome);
+alert(" Yearly income : " + yearlyIncome);
+
+localStorage.setItem("question7", `${dailyIncome} ${weeklyIncome} ${monthlyIncome} ${yearlyIncome}`);
+
+
 
 
 
@@ -203,24 +218,25 @@
 
 
 
-// let loan = prompt("insert loan amount ");
+let loan = prompt("insert loan amount ");
 
-// let installment = prompt ("insert installment amount");
+let installment = prompt ("insert installment amount");
 
-// let interestAmount = prompt ("insert interest amount");
+let interestAmount = prompt ("insert interest amount");
 
-// let monthlyLoan = (loan / installment);
+let monthlyLoan = (loan / installment);
 
-// let monthlyLoanInterest = ((loan * interestAmount) / 100);
+console.log (`monthly loan is: ${monthlyLoan}`)
 
-// let resultLoan = monthlyLoan + monthlyLoanInterest;
+let monthlyLoanInterest = ((loan * interestAmount) / 100);
 
-// alert (monthlyLoan);
+let resultLoan = monthlyLoan + monthlyLoanInterest;
 
-// alert (resultLoan);
+console.log (`monthly installment including interest rate: ${resultLoan}`);
 
 
 
+localStorage.setItem("question8", `${resultLoan}`);
 
 
 
@@ -228,18 +244,20 @@
 // Kullanıcıdan hem kullanıcı adını hem de şifresini alın. Eğer kullanıcı adı "admin" ve şifre "1234" ise “Giriş başarılı” mesajı verin, aksi halde “Kullanıcı adı veya şifre hatalı” deyin.
 
 
-// let username = prompt("enter your username!");
+let username = prompt("enter your username!");
 
-// let password = prompt("enter your password!");
+let password = prompt("enter your password!");
 
 
-// if (username === "admin" && password === "1234") {
-//     alert(`welcome ${username}`);
+if (username === "admin" && password === "1234") {
+    alert(`welcome ${username}`);
 
-// }else if (username === "admin" && password != "1234") {
-//     alert("password is incorrect"); 
+}else if (username === "admin" && password != "1234") {
+    alert("password is incorrect"); 
 
-// } else alert("Username or password is incorrect");
+} else alert("Username or password is incorrect");
+
+localStorage.setItem("question9", `${username} ${password}`);
 
 
 // 10- Net Kar Hesaplama:
@@ -250,18 +268,21 @@
 // anlamadim, Benim max mat bilgisi :D
 
 
-// let salePrice = Number(prompt("enter sale price"));
+let salePrice = Number(prompt("enter sale price"));
 
-// let cost = Number(prompt("enter Product Cost"));
-
-
-// let kdv = ((salePrice) / 100) * 20;
-
-// let withKdv = ((salePrice - cost) + kdv );
-
-// let incomeTax = ((withKdv / 100 ) *  25);
+let cost = Number(prompt("enter Product Cost"));
 
 
-// let result = (((salePrice - cost) + kdv) - incomeTax);
+let kdv = ((salePrice) / 100) * 20;
 
-// console.log (result);
+let withKdv = ((salePrice - cost) + kdv );
+
+let incomeTax = ((withKdv / 100 ) *  25);
+
+
+let result = (((salePrice - cost) + kdv) - incomeTax);
+
+console.log (result);
+
+
+localStorage.setItem("question10", `${result}`);
